d4886ec3f3177a04f74e2bdbbfde07b00f6d7a34
jaysingh8103 <jaypals840@gmail.com>
2025-02-18 04:18:41 +0000

    First

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # main.py (Unoptimized)
 import math
 
+
 def circle_area(radius):
-  if radius < 0:
-    return 0
-  else:
-      area = math.pi*radius**2
-      print(f'Area is {area}')
-      return area
+    if radius < 0:
+        return 0
+    else:
+        area = math.pi * radius**2
+        print(f"Area is {area}")
+        return area
+
 
 circle_area(5)
