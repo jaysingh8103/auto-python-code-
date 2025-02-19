4c6af058cd174a272c62050b6c9a2ccc55fb8ce2
Jaypal Singh Sisodiya <128724391+jaysingh8103@users.noreply.github.com>
2025-02-19 20:22:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os, sys
 
-
- def add_numbers(a,b): return a+b
-  
+def add_numbers(a, b):
+    return a + b
+ 
 def greet(name):
-  print("Hello, " + name)
-
-x= 10;y= 20
-z=add_numbers(x , y )
-print("Sum:",z)
+    print(f"Hello, {name}")
+ 
+x = 10
+y = 20
+z = add_numbers(x, y)
+print("Sum:", z)
 
 greet("Jay")
