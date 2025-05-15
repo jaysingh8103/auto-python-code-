c9696421dfa5ff98d3e51b634d8fd22741b4ce40
Jaypal Singh Sisodiya <128724391+jaysingh8103@users.noreply.github.com>
2025-05-15 13:39:04 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -1,8 +1,8 @@
-import os
-def greet(name)
-    if name = "Alice":
-    print("Hello, " + name)
-  else
-    print("Hi there" + name)
-https://github.com/jaysingh8103/auto-python-code-/edit/main/code2.py
-greet("Bob"     )
+# import os
+# def greet(name)
+#     if name = "Alice":
+#     print("Hello, " + name)
+#   else
+#     print("Hi there" + name)
+# https://github.com/jaysingh8103/auto-python-code-/edit/main/code2.py
+# greet("Bob"     )
